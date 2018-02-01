6b602cdddffb96cd1951157414e72d526cf94d4f
zesthope <zest.h@hopesic.com>
2018-02-01 16:06:32 +0700

    test onChange

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -4,22 +4,28 @@ import App from './App';
 
 const app = shallow(<App />);
 
-it('renders correctly', () => {
-    expect(app).toMatchSnapshot();
-});
-
-it('initializes the `state` with an empty list of gifts', () => {
-    expect(app.state().gifts).toEqual([]);
-});
-
-it('adds a new gift to `state` when clicking the `add gift` button', () => {
-    app.find('.btn-add').simulate('click');
-
-    expect(app.state().gifts).toEqual([ {id: 1} ]);
-});
-
-it('adds a new gift to a rendered list when clicking the `add gift` button', () => {
-    app.find('.btn-add').simulate('click');
-
-    expect(app.find('.gift-list').children().length).toEqual(2);
+describe('App', () => {
+    it('renders correctly', () => {
+        expect(app).toMatchSnapshot();
+    });
+    
+    it('initializes the `state` with an empty list of gifts', () => {
+        expect(app.state().gifts).toEqual([]);
+    });
+    
+    describe('When clicking the `add gift` button', () => {
+        beforeEach(() => {
+            app.find('.btn-add').simulate('click');
+        });
+        afterEach(() => {
+            app.setState({ gifts: [] });
+        });
+        it('adds a new gift to `state` when clicking the `add gift` button', () => {
+            expect(app.state().gifts).toEqual([ {id: 1} ]);
+        });
+        
+        it('adds a new gift to a rendered list when clicking the `add gift` button', () => {
+            expect(app.find('.gift-list').children().length).toEqual(1);
+        });
+    });
 });
